3c2bedf832f2a48dc2492509daeb7f9d70e4a8b1
gojowq <parisplebeu@gmail.com>
2025-06-24 18:02:27 -0300

    Adiciona install.sh para instalação automática

--- install.sh
+++ install.sh
@@ -1,4 +1,16 @@
-#!/data/data/com.termux/files/usr/bin/bash
+#!/bin/bash
+
+echo -e "\033[1;32m[+] Instalando dependências...\033[0m"
+pkg update -y && pkg upgrade -y
+pkg install git php android-tools -y
+
+echo -e "\n\033[1;32m[+] Clonando repositório...\033[0m"
+rm -rf gojo-scanner
+git clone https://github.com/gojowq/gojo-scanner
+cd gojo-scanner
+
+echo -e "\n\033[1;32m[+] Executando Gojo.php...\033[0m"
+php Gojo.php#!/data/data/com.termux/files/usr/bin/bash
 
 echo -e "\033[93m[+] Instalando dependências...\033[0m"
 
